fb7794da35ae964e0fa609dca2838b84d4e97ba5
2038prob <44622018+2038prob@users.noreply.github.com>
2021-02-05 07:44:11 -0500

    main function changed

--- AY21_ws/src/igvc_sw/lane_following/main.py
+++ AY21_ws/src/igvc_sw/lane_following/main.py
@@ -33,14 +33,7 @@ LEAD_Y_SCALE = 10
 
 bridge = CvBridge()
 
-def lane_following(image):
-  #matplotlib.use('Agg')
-  camerafile = image#sys.argv[1]
-  supercombo = load_model('supercombo.keras')
-
-  # print(supercombo.summary())
-
-  def frames_to_tensor(frames):                                                                                               
+def frames_to_tensor(frames):                                                                                               
     H = (frames.shape[1]*2)//3                                                                                                
     W = frames.shape[2]                                                                                                       
     in_img1 = np.zeros((frames.shape[0], 6, H//2, W//2), dtype=np.uint8)                                                      
@@ -53,60 +46,67 @@ def lane_following(image):
     in_img1[:, 5] = frames[:, H+H//4:H+H//2].reshape((-1, H//2,W//2))
     return in_img1
 
-  imgs_med_model = np.zeros((2, 384, 512), dtype=np.uint8)
-  state = np.zeros((1,512))
-  desire = np.zeros((1,8))
+def lane_following(image):
+    #matplotlib.use('Agg')
+    camerafile = image#sys.argv[1]
+    supercombo = load_model('supercombo.keras')
 
-  cap = cv2.VideoCapture(camerafile)
+    # print(supercombo.summary())
 
-  x_left = x_right = x_path = np.linspace(0, 192, 192)
-  (ret, previous_frame) = cap.read()
-  if not ret:
-    exit()
-  else:
-    img_yuv = cv2.cvtColor(previous_frame, cv2.COLOR_BGR2YUV_I420)
-    imgs_med_model[0] = transform_img(img_yuv, from_intr=eon_intrinsics, to_intr=medmodel_intrinsics, yuv=True,
-                                      output_size=(512,256))
+    imgs_med_model = np.zeros((2, 384, 512), dtype=np.uint8)
+    state = np.zeros((1,512))
+    desire = np.zeros((1,8))
 
-  filtered = 600
-  alpha = 0.1
-  while True:
-    plt.clf()
-    plt.title("lanes and path")
-    plt.xlim(0, 1200)
-    plt.ylim(800, 0)
-    (ret, current_frame) = cap.read()
+    cap = cv2.VideoCapture(camerafile)
+
+    x_left = x_right = x_path = np.linspace(0, 192, 192)
+    (ret, previous_frame) = cap.read()
     if not ret:
-        break
-    frame = current_frame.copy()
-    img_yuv = cv2.cvtColor(current_frame, cv2.COLOR_BGR2YUV_I420)
-    imgs_med_model[1] = transform_img(img_yuv, from_intr=eon_intrinsics, to_intr=medmodel_intrinsics, yuv=True,
-                                      output_size=(512,256))
-    frame_tensors = frames_to_tensor(np.array(imgs_med_model)).astype(np.float32)/128.0 - 1.0
+        exit()
+    else:
+        img_yuv = cv2.cvtColor(previous_frame, cv2.COLOR_BGR2YUV_I420)
+        imgs_med_model[0] = transform_img(img_yuv, from_intr=eon_intrinsics, to_intr=medmodel_intrinsics, yuv=True,
+                                        output_size=(512,256))
 
-    inputs = [np.vstack(frame_tensors[0:2])[None], desire, state]
-    outs = supercombo.predict(inputs)
-    parsed = parser(outs)
-    # Important to refeed the state
-    state = outs[-1]
-    pose = outs[-2]   # For 6 DoF Callibration
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    plt.imshow(frame)
-    new_x_left, new_y_left = transform_points(x_left, parsed["lll"][0])
-    new_x_right, new_y_right = transform_points(x_left, parsed["rll"][0])
-    new_x_path, new_y_path = transform_points(x_left, parsed["path"][0])
-    error = 600 - new_x_path[0]
-    deg = (new_x_path[-1] - new_x_path[0])
-    filtered = alpha*error + (1-alpha)*filtered
-    # print(filtered) # heading and the cross track error error = middle - new_x_path[0]
-    # print("---------------------------")
-    plt.plot(new_x_left, new_y_left, label='transformed', color='w', linewidth=4)
-    plt.plot(new_x_right, new_y_right, label='transformed', color='w', linewidth=4)
-    plt.plot(new_x_path, new_y_path, label='transformed', color='green', linewidth=4)
-    imgs_med_model[0]=imgs_med_model[1]
-    plt.pause(0.001)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-          break
+    filtered = 600
+    alpha = 0.1
+    while True:
+        plt.clf()
+        plt.title("lanes and path")
+        plt.xlim(0, 1200)
+        plt.ylim(800, 0)
+        (ret, current_frame) = cap.read()
+        if not ret:
+            break
+        frame = current_frame.copy()
+        img_yuv = cv2.cvtColor(current_frame, cv2.COLOR_BGR2YUV_I420)
+        imgs_med_model[1] = transform_img(img_yuv, from_intr=eon_intrinsics, to_intr=medmodel_intrinsics, yuv=True,
+                                        output_size=(512,256))
+        frame_tensors = frames_to_tensor(np.array(imgs_med_model)).astype(np.float32)/128.0 - 1.0
+
+        inputs = [np.vstack(frame_tensors[0:2])[None], desire, state]
+        outs = supercombo.predict(inputs)
+        parsed = parser(outs)
+        # Important to refeed the state
+        state = outs[-1]
+        pose = outs[-2]   # For 6 DoF Callibration
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        plt.imshow(frame)
+        new_x_left, new_y_left = transform_points(x_left, parsed["lll"][0])
+        new_x_right, new_y_right = transform_points(x_left, parsed["rll"][0])
+        new_x_path, new_y_path = transform_points(x_left, parsed["path"][0])
+        error = 600 - new_x_path[0]
+        deg = (new_x_path[-1] - new_x_path[0])
+        filtered = alpha*error + (1-alpha)*filtered
+        # print(filtered) # heading and the cross track error error = middle - new_x_path[0]
+        # print("---------------------------")
+        plt.plot(new_x_left, new_y_left, label='transformed', color='w', linewidth=4)
+        plt.plot(new_x_right, new_y_right, label='transformed', color='w', linewidth=4)
+        plt.plot(new_x_path, new_y_path, label='transformed', color='green', linewidth=4)
+        imgs_med_model[0]=imgs_med_model[1]
+        plt.pause(0.001)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
 
   #plt.show()
   
@@ -136,29 +136,29 @@ class image_feature:
         #### direct conversion to CV2 ####
         cv_image = bridge.imgmsg_to_cv2(ros_data, "bgr8")
         
-        ############################################################################################
-        #### Feature detectors using CV2 #### 
-        # "","Grid","Pyramid" + 
-        # "FAST","GFTT","HARRIS","MSER","ORB","SIFT","STAR","SURF"
-        feat_det = cv2.GFTTDetector.create()
-        time1 = time.time()
+        # ############################################################################################
+        # #### Feature detectors using CV2 #### 
+        # # "","Grid","Pyramid" + 
+        # # "FAST","GFTT","HARRIS","MSER","ORB","SIFT","STAR","SURF"
+        # feat_det = cv2.GFTTDetector.create()
+        # time1 = time.time()
 
-        # convert np image to grayscale
-        greyIm = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-        featPoints = feat_det.detect(greyIm)
-        time2 = time.time()
-        if VERBOSE :
-            print '%s detector found: %s points in: %s sec.'%(method,
-                len(featPoints),time2-time1)
+        # # convert np image to grayscale
+        # greyIm = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        # featPoints = feat_det.detect(greyIm)
+        # time2 = time.time()
+        # if VERBOSE :
+        #     print '%s detector found: %s points in: %s sec.'%(method,
+        #         len(featPoints),time2-time1)
 
-        for featpoint in featPoints:
-            x,y = featpoint.pt
-            cv2.circle(cv_image,(int(x),int(y)), 3, (0,0,255), -1)
-        #############################################################################################
+        # for featpoint in featPoints:
+        #     x,y = featpoint.pt
+        #     cv2.circle(cv_image,(int(x),int(y)), 3, (0,0,255), -1)
+        # #############################################################################################
         
         lane_following(cv_image)
-        cv2.imshow('cv_img', cv_image)
-        cv2.waitKey(2)
+        # cv2.imshow('cv_img', cv_image)
+        # cv2.waitKey(2)
 
         #### Create CompressedIamge ####
         msg = CompressedImage()
