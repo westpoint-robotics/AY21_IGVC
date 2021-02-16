48c249b6a0b79acef31e6aaa5105930315cee5dc
User1 Nuvo <dominic.larkin@westpoint.edu>
2021-02-16 10:17:49 -0500

    top left corner danm it

--- AY21_ws/src/igvc_sw/lane_following/main.py
+++ AY21_ws/src/igvc_sw/lane_following/main.py
@@ -34,72 +34,116 @@ MAX_REL_V = 10.
 
 LEAD_X_SCALE = 10
 LEAD_Y_SCALE = 10
-#width = 1920
-#height = 1440
+WIDTH = 1920
+HEIGHT = 1440
 
 bridge = CvBridge()
 
+# We need a place to keep two separate consecutive image frames
+# since that's what SuperCombo uses
+previous_frame = np.zeros((HEIGHT, WIDTH), dtype=np.uint8)
+current_frame = np.zeros((HEIGHT, WIDTH), dtype=np.uint8)
+
 crossTrackError = rospy.Publisher('/cross_track_error', Float32, queue_size=1)
 
-def frames_to_tensor(frames):                                                                                               
-    H = (frames.shape[1]*2)//3                                                                                                
-    W = frames.shape[2]                                                                                                       
-    in_img1 = np.zeros((frames.shape[0], 6, H//2, W//2), dtype=np.uint8)                                                      
-                                                                                                                              
-    in_img1[:, 0] = frames[:, 0:H:2, 0::2]                                                                                    
-    in_img1[:, 1] = frames[:, 1:H:2, 0::2]                                                                                    
-    in_img1[:, 2] = frames[:, 0:H:2, 1::2]                                                                                    
-    in_img1[:, 3] = frames[:, 1:H:2, 1::2]                                                                                    
-    in_img1[:, 4] = frames[:, H:H+H//4].reshape((-1, H//2,W//2))                                                              
-    in_img1[:, 5] = frames[:, H+H//4:H+H//2].reshape((-1, H//2,W//2))
+def frames_to_tensor(frames):
+    #print frames.shape
+
+    """
+    Dr. Gonzalez added shape transformation
+    """
+    temp = np.zeros((2, 256, 512), dtype=np.uint8)
+    temp[0] = cv2.resize(frames[0], (512, 384))[64:320, :]
+    temp[1] = cv2.resize(frames[1], (512, 384))[64:320, :]
+    frames = temp
+    #print frames.shape
+    W = int(frames.shape[2])
+    H = int(frames.shape[1])
+
+    in_img1 = np.zeros((frames.shape[0], 6, H//2, W//2), dtype=np.uint8)
+
+    """
+    Dr. Gonzalez added different start and end
+    """
+    in_img1[:, 0] = frames[:, 0:H:2, 0::2] #odds, odds
+    in_img1[:, 1] = frames[:, 1:H:2, 0::2] #evens, odds
+    in_img1[:, 2] = frames[:, 0:H:2, 1::2] #oods, evens
+    in_img1[:, 3] = frames[:, 1:H:2, 1::2] #evens, evens
+    #print frames.shape
+    #print frames[:, H//2:H//2+H//4].shape
+    #print frames[:, H//2+H//4:H//2+H//2].shape
+    in_img1[:, 4] = frames[:, H//2:H//2+H//4].reshape((-1, H//2,W//2))
+    in_img1[:, 5] = frames[:, H//2+H//4:H//2+H//2].reshape((-1, H//2,W//2))
+
+    #print in_img1.shape
     return in_img1
 
 def lane_following(image):
     global supercombo, graph
     # print(supercombo.summary())
-    imgs_med_model = np.zeros((2, 384, 512), dtype=np.uint8)
-    state = np.zeros((1,512))
+    #imgs_med_model = np.zeros((2, 256, 512), dtype=np.uint8)
+    imgs_med_model = np.zeros((2, HEIGHT, WIDTH), dtype=np.uint8)
+    state = np.zeros((1,512)) #################################################
+    #state = np.zeros((1,WIDTH))
     desire = np.zeros((1,8))
 
-    cap = bridge.imgmsg_to_cv2(image, desired_encoding="bgr8")
-
     x_left = x_right = x_path = np.linspace(0, 192, 192)
-    previous_frame = cap
 
-    img_yuv = cv2.cvtColor(previous_frame, cv2.COLOR_BGR2YUV_I420)
-    imgs_med_model[0] = transform_img(img_yuv, from_intr=eon_intrinsics, to_intr=medmodel_intrinsics, yuv=True,
-                                        output_size=(512,256))
+    cap = bridge.imgmsg_to_cv2(image, desired_encoding="bgr8")
+    # print cap.shape # (1440, 1920, 3)
 
-    filtered = 600
+    # filtered = 600
+    filtered = WIDTH//2
     alpha = 0.1
     plt.clf()
     plt.title("lanes and path")
-    plt.xlim(0, 1200)
-    # plt.xlim(0, 1920)
-    plt.ylim(800, 0)
-    # plt.ylim(1440, 0)
+    # plt.xlim(0, 1200)
+    plt.xlim(0, WIDTH)
+    # plt.ylim(800, 0)
+    plt.ylim(HEIGHT, 0)
 
     current_frame = cap
     frame = current_frame.copy()
     img_yuv = cv2.cvtColor(current_frame, cv2.COLOR_BGR2YUV_I420)
-    imgs_med_model[1] = transform_img(img_yuv, from_intr=eon_intrinsics, to_intr=medmodel_intrinsics, yuv=True, output_size=(512,256))
-    frame_tensors = frames_to_tensor(np.array(imgs_med_model)).astype(np.float32)/128.0 - 1.0
-    #inputs = [np.vstack(frame_tensors[0:2])[None], desire, state]
-    inputs = [np.vstack(frame_tensors[0:2])[None], np.zeros((1,8)), state]
+    # frame is a list of 1440 x 1920
+    # imgs_med_model[1] = transform_img(img_yuv, from_intr=eon_intrinsics, to_intr=medmodel_intrinsics, yuv=True, output_size=(512,256))
+    imgs_med_model[1] = transform_img(img_yuv, from_intr=eon_intrinsics, to_intr=medmodel_intrinsics, yuv=True, output_size=(WIDTH,WIDTH/2)) ##########################################
+    
+    frame_tensors = frames_to_tensor(np.array(imgs_med_model)).astype(np.float32)/128.0 - 1.0 ###################################################
+    # print frame_tensors.shape # (2, 6, 128, 960)
+    
+    inputs = [np.vstack(frame_tensors[0:2])[None], desire, state] #########################################################
+    # inputs = [np.vstack(frame_tensors[0:2])[None], np.zeros((1,8)), state]
+    # print len(frame_tensors[0][0])
+
     with graph.as_default():
         set_session(sess)
         outs = supercombo.predict(inputs)
+
+    previous_frame = current_frame
+    
     #K.clear_session()
     parsed = parser(outs)
     # Important to refeed the state
     state = outs[-1]
     pose = outs[-2]   # For 6 DoF Callibration
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
     plt.imshow(frame)
+
     new_x_left, new_y_left = transform_points(x_left, parsed["lll"][0])
-    new_x_right, new_y_right = transform_points(x_left, parsed["rll"][0])
-    new_x_path, new_y_path = transform_points(x_left, parsed["path"][0])
-    error = 600 - new_x_path[0]
+    new_x_right, new_y_right = transform_points(x_right, parsed["rll"][0])
+    new_x_path, new_y_path = transform_points(x_path, parsed["path"][0])
+    # new_x_left = [x*2 for x in new_x_left]
+    # new_y_left = [x*2 for x in new_y_left]
+    # new_x_right = [x*2 for x in new_x_right]
+    # new_y_right = [x*2 for x in new_y_right]
+    # new_x_path = [x*2 for x in new_x_path]
+    # new_y_path = [x*2 for x in new_y_path]
+
+    error = WIDTH//2 - new_x_path[0]
+    #print 'new_x_path[0]: ', new_x_path[0]
+    #print 'error [pixels]: ', error
     # deg = (new_x_path[-1] - new_x_path[0])
     filtered = alpha*error + (1-alpha)*filtered
     crossTrackError.publish(filtered)
