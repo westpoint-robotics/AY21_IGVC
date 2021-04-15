47a6e6c9a07ae552dad7d9f70ef92ce5de8e0d7d
HyunJin <46986415+ArmaVirum@users.noreply.github.com>
2021-04-15 11:39:44 -0400

    Update conversions.py

--- catkin_ws/src/ay21_igvc_catkin/AY21_ws/src/igvc_sw/conversions.py
+++ catkin_ws/src/ay21_igvc_catkin/AY21_ws/src/igvc_sw/conversions.py
@@ -1,42 +1,38 @@
 import math
 
-image_width = 1920
-#fov is in degrees
-camera_fov = 120
-fov = math.radians(camera_fov)
+def triangulate(pxl=100, pyl=100, pxr=150, pyr=150):
+    image_width = 1920
+    #fov is in degrees
+    camera_fov = 120
+    fov = math.radians(camera_fov)
 
-#Left-side location
-pxl = 100
-pyl = 100
+    #Delta x and y
+    dpx = pxr - pxl
+    dpy = pyr - pyl
 
-#right-side location
-pxr = 800
-pyr = 800
+    #Center of stopsign
+    centerx = dpx/2
+    centery = dpy/2
 
-#Delta x and y
-dpx = pxr - pxl
-dpy = pyr - pyl
+    #ppr
+    ppr = image_width/fov
 
-#Center of stopsign
-centerx = dpx/2
-centery = dpy/2
+    #radian conversions
+    rad_x = centerx/ppr
+    rad_y = centery/ppr
+    drad_x = dpx/ppr
+    drad_y = dpy/ppr
 
-#ppr
-ppr = image_width/fov
+    #Distance to stop sign
+    r = 0.75/drad_x
 
-#radian conversions
-rad_x = centerx/ppr
-rad_y = centery/ppr
-drad_x = dpx/ppr
-drad_y = dpy/ppr
+    #delta x and delta y the 
+    delta_x = r*math.cos(rad_x)
+    delta_y = r*math.sin(rad_x)
+    return (delta_x,delta_y)
 
-#Distance to stop sign
-r = 0.75/drad_x
-
-#delta x and delta y
-delta_x = r*math.cos(rad_x)
-delta_y = r*math.sin(rad_x)
-
-print("Here is the distance of the stop sign from the car.")
-print(delta_x)
-print(delta_y)
+def test():
+    delta_x,delta_y = triangulate()
+    print("Here is the distance of the stop sign from the car.")
+    print(delta_x)
+    print(delta_y)
